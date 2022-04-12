5fcee2b2d4646f20a8e1092cf52d59ee0fb599c0
Mateo Estrada Ramirez <mateoestradaramirez2001@gmail.com>
2022-04-12 12:16:16 -0500

    Corregir el repo de admin de hotel

--- persistencia/src/main/java/co/edu/uniquindio/proyecto/repositorios/AdministradorHotelRepo.java
+++ persistencia/src/main/java/co/edu/uniquindio/proyecto/repositorios/AdministradorHotelRepo.java
@@ -3,7 +3,7 @@ package co.edu.uniquindio.proyecto.repositorios;
 import co.edu.uniquindio.proyecto.entidades.Persona_Administrador_Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdministradorHotelRepo extends JpaRepository <Persona_Administrador_Hotel, Integer>
+public interface AdministradorHotelRepo extends JpaRepository <Persona_Administrador_Hotel, String>
 {
     Persona_Administrador_Hotel findByCedula(String cedula);
 }
