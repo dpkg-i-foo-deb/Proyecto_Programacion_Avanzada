7b17e945f66a9f282aeced60fb92361d4dc5f9cd
Mateo Estrada Ramirez <mateoestradaramirez2001@gmail.com>
2022-04-19 14:51:12 -0500

    Agregar el servicio e implementacion de la ciudad

---
+++ negocio/src/main/java/co/edu/uniquindio/proyecto/servicios/ICiudadServicio.java
@@ -0,0 +1,19 @@
+package co.edu.uniquindio.proyecto.servicios;
+
+import co.edu.uniquindio.proyecto.entidades.Ciudad;
+import co.edu.uniquindio.proyecto.servicios.excepciones.CiudadException;
+
+import java.util.List;
+
+public interface ICiudadServicio
+{
+    Ciudad registrarCiudad(Ciudad ciudad);
+
+    Ciudad editarCiudad(Ciudad ciudad) throws CiudadException;
+
+    void eliminarCiudad(Ciudad ciudad);
+
+    Ciudad obtenerCiudad(Integer codigo) throws CiudadException;
+
+    List<Ciudad> obtenerCiudades();
+}
---
+++ negocio/src/main/java/co/edu/uniquindio/proyecto/servicios/implementacion/CiudadImpl.java
@@ -0,0 +1,55 @@
+package co.edu.uniquindio.proyecto.servicios.implementacion;
+
+import co.edu.uniquindio.proyecto.entidades.Ciudad;
+import co.edu.uniquindio.proyecto.repositorios.CiudadRepo;
+import co.edu.uniquindio.proyecto.servicios.ICiudadServicio;
+import co.edu.uniquindio.proyecto.servicios.excepciones.CiudadException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CiudadImpl implements ICiudadServicio
+{
+    private final CiudadRepo ciudadRepo;
+
+    public CiudadImpl(CiudadRepo ciudadRepo) { this.ciudadRepo = ciudadRepo; }
+
+    @Override
+    public Ciudad registrarCiudad(Ciudad ciudad)
+    {
+        return ciudadRepo.save(ciudad);
+    }
+
+    @Override
+    public Ciudad editarCiudad(Ciudad ciudad) throws CiudadException
+    {
+        if(ciudadRepo.findById(ciudad.getCodigoCiudad()).orElse(null)==null)
+            throw new CiudadException("La ciudad no existe");
+
+        return ciudadRepo.save(ciudad);
+    }
+
+    @Override
+    public void eliminarCiudad(Ciudad ciudad)
+    {
+        ciudadRepo.delete(ciudad);
+    }
+
+    @Override
+    public Ciudad obtenerCiudad(Integer codigo) throws CiudadException
+    {
+        Ciudad ciudad = ciudadRepo.findById(codigo).orElse(null);
+
+        if(ciudad==null)
+            throw new CiudadException("La ciudad no existe");
+
+        return ciudad;
+    }
+
+    @Override
+    public List<Ciudad> obtenerCiudades()
+    {
+        return ciudadRepo.findAll();
+    }
+}
